5fe307c81f1b8cc7a5dd218c50170c563b2dd5eb
lucas <lc45648siete@gmail.com>
2022-10-05 21:55:02 -0300

    1.7.1 Bug Fixes

--- Clases/ConexionBD.py
+++ Clases/ConexionBD.py
@@ -12,7 +12,7 @@ class Conexiones:
         self.miConexion.close()   
     
     def crearTablas(self):
-        conexion = ConexionBD.Conexiones()
+        conexion = Conexiones()
         conexion.abrirConexion()
         conexion.miCursor.execute("DROP TABLE IF EXISTS AUTOMOVILES")
         conexion.miCursor.execute("CREATE TABLE AUTOMOVILES (id_automovil INTEGER PRIMARY KEY , marca  VARCHAR(30) ,modelo  VARCHAR(30),precio FLOAT NOT NULL, cantidadDisponibles INTEGER NOT NULL,UNIQUE(marca,modelo))")    
